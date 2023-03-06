93c42b0b1ad8e54323a29bf6d34ab5292660d46e
Joy <parkerjn90@gmail.com>
2023-03-06 10:30:33 -0500

    edit header and info css

--- src/modules/header.jsx
+++ src/modules/header.jsx
@@ -4,8 +4,8 @@ import Portrait from '../Me.jpeg';
 const Header = () => {
   return <div>
     <div className="header-flex-2/3">
-      <h1>Joy Parker</h1>
-      <h2>Full Stack Software Developer</h2>
+      <h1>Joy Parker &#32;	</h1><h1 className="subtitle">&#32; - Full Stack Software Developer</h1>
+      {/* <h2>Full Stack Software Developer</h2> */}
     </div>
     <div className="header-flex-1/3">
       <img className="me" src={Portrait} alt="self-portrait of Joy Parker"/>
