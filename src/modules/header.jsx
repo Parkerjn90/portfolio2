350f29e9b805e165f538d7767427e4ffa5c51c05
Joy <parkerjn90@gmail.com>
2023-03-06 10:13:47 -0500

    add header tags and content

--- src/modules/header.jsx
+++ src/modules/header.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Portrait from '../Me.jpeg';
 
 const Header = () => {
-  return <h1>Header</h1>
+  return <div>
+    <div className="header-flex-2/3">
+      <h1>Joy Parker</h1>
+      <h2>Full Stack Software Developer</h2>
+    </div>
+    <div className="header-flex-1/3">
+      <img className="me" src={Portrait} alt="self-portrait of Joy Parker"/>
+    </div>
+  </div>
 }
 
 export default Header;
