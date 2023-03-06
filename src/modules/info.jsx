64792c50c7c32dcdb3a6f01dbe46fb1088ccceda
Joy <parkerjn90@gmail.com>
2023-03-06 10:20:46 -0500

    info tags and content

--- src/modules/info.jsx
+++ src/modules/info.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
 const Info = () => {
-  return <h2>Info</h2>
+  return <div className="flex-box-1">
+    <h2>Info</h2>
+    <div className="inner-text">
+      <p>
+      Front End | JavaScript (ES5 and ES6), React, HTML5, CSS3, Styled-Components, SASS, Axios, AJAX <br/>
+      Back End | Node.js, Express, MySQL, SQLite, MongoDB, Mongoose, PostgreSQL, PGAdmin, RESTful API Development<br/>
+      Testing/Deployment | Jest, Mocha, Chai, AWS: EC2<br/>
+      Developer Tools | Vim, Git, npm, yarn, Webpack, Babel, Postman
+      </p>
+      <p>I spent the first 8 years of my career building joy for working
+        with people of diverse backgrounds - I was a high school French teacher.
+        Through this experience I also continued to develop a love I&#39;ve had my whole life of
+        solving puzzles and complex problems. I should have been a math teacher.
+        When the time came for me to transition into a new career, it was clear that
+        I needed something that would challenge me and continue to allow me to work with
+        all manner of people and solve all manner of problems. Coding has allowed me to work
+        with people from all around the globe without leaving my desk. I find joy solving coding
+        challenges -  I even like hunting bugs. I have worked hard to become an effective pair
+        programming partner. I participate fully in retrospectives. I can work on agile teams
+        and ensure everyone&#39;s voice is included in the discussion.
+        </p>
+      </div>
+    </div>
 }
 
 export default Info;
