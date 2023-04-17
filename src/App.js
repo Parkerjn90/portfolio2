4bb8572bc338a48dc450e8777b689da1f3fcc237
Joy <parkerjn90@gmail.com>
2023-04-17 17:03:24 -0400

    sticky header added and styled, need to move rest of content BELOW header div

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { React, useState } from 'react';
 // import './App.css';
-import { ThemeProvider, Box, createTheme } from '@mui/system';
-// import { Box } from '@mui/joy';
+// import { ThemeProvider, Box, createTheme } from '@mui/system';
+// import Grid from '@mui/joy/Grid';
 import Header from './modules/header.jsx';
 import Info from './modules/info.jsx';
 import Experience from './modules/experience.jsx';
@@ -10,72 +10,59 @@ import InTheWorks from './modules/inTheWorks.jsx';
 
 function App() {
 
-  const theme = createTheme({
-    palette: {
-      background: {
-        paper: 'url("https://i.ibb.co/93Bpmjt/Profile-Main.png")',
-      },
-      text: {
-        primary: '#173A5E',
-        secondary: '#46505A',
-      },
-      action: {
-        active: '#001E3C',
-      },
-      success: {
-        dark: '#009688',
-      },
-    },
-  });
-
   const [experience] = useState([
-    {title: 'Questions and Answers Service',
-    goal: 'Front-End clothing retail application',
-    site: 'github.com/Team-Pharos/FEC_Lighthouse',
-    description: [
-      'Implemented responsive search bar functionality within a  React component',
-      'Optimized slow API response rate by compacting multiple Axios calls into one, resulting in five times more data returned in the same amount of time',
-      'Developed reusable containers across the application using styled-components'
-    ],
-    gif: 'https://i.ibb.co/rvXHvLd/pharos.gif'
+    {
+      title: 'Questions and Answers Service',
+      goal: 'Front-End clothing retail application',
+      site: 'github.com/Team-Pharos/FEC_Lighthouse',
+      description: [
+        'Implemented responsive search bar functionality within a  React component',
+        'Optimized slow API response rate by compacting multiple Axios calls into one, resulting in five times more data returned in the same amount of time',
+        'Developed reusable containers across the application using styled-components'
+      ],
+      gif: 'https://i.ibb.co/rvXHvLd/pharos.gif'
     },
-    {title: 'E-commerce Product API',
-    goal: 'Data migration and scaling',
-    site: 'github.com/Great-Blue-Herons/Product-Overview',
-    description: [
-      'Designed schema in PostgreSQL to allow for efficient data migration',
-      'Wrote queries within PGAdmin to quickly aggregate data resulting in an initial average of 346 requests per second',
-      'Deployed to AWS server and increased average response time to 1000 requests per second'
-    ],
-    gif: 'https://i.ibb.co/hY61TGL/heron.png'
+    {
+      title: 'E-commerce Product API',
+      goal: 'Data migration and scaling',
+      site: 'github.com/Great-Blue-Herons/Product-Overview',
+      description: [
+        'Designed schema in PostgreSQL to allow for efficient data migration',
+        'Wrote queries within PGAdmin to quickly aggregate data resulting in an initial average of 346 requests per second',
+        'Deployed to AWS server and increased average response time to 1000 requests per second'
+      ],
+      gif: 'https://i.ibb.co/hY61TGL/heron.png'
     },
-    {title: 'Games Collective',
-    goal: 'Full-stack application to aggregate video games, manage personal lists, search database, and participate in related threads',
-    site: 'github.com/Jelly-Donut-Fish/GamesCollective',
-    description: [
-      'Led a team of eight developers to create a multi-page, full-stack application for video game management',
-      'Leveraged React, Redux, and Axios to allow users to manage collections and view detailed information',
-      'Implemented SASS for site-wide styling in dark mode to improve accessibility'
-    ],
-    gif: 'https://i.ibb.co/rymLwkR/jellyfish.gif',
+    {
+      title: 'Games Collective',
+      goal: 'Full-stack application to aggregate video games, manage personal lists, search database, and participate in related threads',
+      site: 'github.com/Jelly-Donut-Fish/GamesCollective',
+      description: [
+        'Led a team of eight developers to create a multi-page, full-stack application for video game management',
+        'Leveraged React, Redux, and Axios to allow users to manage collections and view detailed information',
+        'Implemented SASS for site-wide styling in dark mode to improve accessibility'
+      ],
+      gif: 'https://i.ibb.co/rymLwkR/jellyfish.gif',
     },
   ]);
 
   const [jobs] = useState([
-    {jobTitle: 'Lead Software Engineer Immersive Resident',
-    company: 'Galvanize Inc.',
-    dateEmployed: 'Jul 2022 - Jan 2023',
-    activities: [
-      'Instructed 100+ junior engineers in JavaScript, React, Node.js, and Database Management Systems',
-      'Conducted daily code review to help students solidify CS concepts and discuss potential optimizations',
-      'Refactored the Hack Reactor curriculum in collaboration with other engineers and educators',
-      'Managed a diverse team of Software Engineering Immersive Residents to analyze student progress and ensure future success',
-      'Contributed to academic curriculum codebase and led retrospectives for 40+ engineers on a weekly basis',
-      'Interviewed, hired, and trained new software developers',
-      'Established company-wide standard practices by creating and implementing new documents, resulting in increased efficiency and consistency in student support'
+    {
+      jobTitle: 'Lead Software Engineer Immersive Resident',
+      company: 'Galvanize Inc.',
+      dateEmployed: 'Jul 2022 - Jan 2023',
+      activities: [
+        'Instructed 100+ junior engineers in JavaScript, React, Node.js, and Database Management Systems',
+        'Conducted daily code review to help students solidify CS concepts and discuss potential optimizations',
+        'Refactored the Hack Reactor curriculum in collaboration with other engineers and educators',
+        'Managed a diverse team of Software Engineering Immersive Residents to analyze student progress and ensure future success',
+        'Contributed to academic curriculum codebase and led retrospectives for 40+ engineers on a weekly basis',
+        'Interviewed, hired, and trained new software developers',
+        'Established company-wide standard practices by creating and implementing new documents, resulting in increased efficiency and consistency in student support'
       ],
     },
-    {jobTitle: 'Clinical Screening Specialist',
+    {
+      jobTitle: 'Clinical Screening Specialist',
       company: 'CastleBranch',
       dateEmployed: 'Jun 2020 - Feb 2022',
       activities: [
@@ -83,16 +70,18 @@ function App() {
         'Promoted from Tier 1 to Tier 2 level reviewer within 4 months of completing training'
       ],
     },
-    {jobTitle: 'French Teacher, Yearbook, Testing Coordinator, Department Chair',
-        company: 'Graham High School',
-        dateEmployed: 'Aug 2015 - Jun 2020',
-        activities: [
-          'Designed daily lessons for students at various levels of competency in the French language',
-          'Trained teachers in national/state-wide testing practices to maintain student confidentiality and increase percent of students tested for the school',
-          'Trained other teachers in educational technology to increase student engagement and skill-retention'
-        ],
+    {
+      jobTitle: 'French Teacher, Yearbook, Testing Coordinator, Department Chair',
+      company: 'Graham High School',
+      dateEmployed: 'Aug 2015 - Jun 2020',
+      activities: [
+        'Designed daily lessons for students at various levels of competency in the French language',
+        'Trained teachers in national/state-wide testing practices to maintain student confidentiality and increase percent of students tested for the school',
+        'Trained other teachers in educational technology to increase student engagement and skill-retention'
+      ],
     },
-    {jobTitle: 'French Teacher',
+    {
+      jobTitle: 'French Teacher',
       company: 'Fred T. Foard High School',
       dateEmployed: 'Jan 2013 - Jun 2015',
       activities: [
@@ -115,20 +104,17 @@ function App() {
   });
 
   return (
-    <ThemeProvider theme={theme}>
-      <Box variant="outlined" m="auto">
-        <Box variant="outlined" >
-        <header className="App-header">
-          <Header></Header>
-          <Info></Info>
-          <Experience experience={experience}></Experience>
-          <Jobs jobs={jobs}></Jobs>
-          <InTheWorks projects={projects}></InTheWorks>
-        </header>
-        </Box>
-      </Box>
-    </ThemeProvider>
-
+    <>
+      <Header></Header>
+      <div className="main">
+        <div className="inner">
+          <div id="info"><Info></Info></div>
+          <div id="experience"><Experience experience={experience}></Experience></div>
+          <div id="jobs"><Jobs jobs={jobs}></Jobs></div>
+          <div id="works"><InTheWorks projects={projects}></InTheWorks></div>
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/modules/header.jsx
+++ src/modules/header.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
+import Grid from '@mui/joy/Grid';
 
 const Header = () => {
-  return <div>
-    <div className="header-flex-2/3">
-      <h1>Joy Parker &#32;	</h1><h1 className="subtitle">&#32; - Full Stack Software Developer</h1>
-      <p>
-      Front End | JavaScript (ES5 and ES6), React, HTML5, CSS3, Styled-Components, SASS, Axios, AJAX <br/>
-      Back End | Node.js, Express, MySQL, SQLite, MongoDB, Mongoose, PostgreSQL, PGAdmin, RESTful API Development<br/>
-      Testing/Deployment | Jest, Mocha, Chai, AWS: EC2<br/>
-      Developer Tools | Vim, Git, npm, yarn, Webpack, Babel, Postman
-      </p>
+  return (
+    <div className="header">
+      <div className="innerHeader">
+        <Grid container spacing={0} sx={{ flexGrow: 1 }}>
+          <Grid xs={8}>
+            <h1>Joy Parker &#32;	</h1><h1 className="subtitle">&#32; - Full Stack Software Developer</h1>
+            <p style={{ color: "#b1bf9e", fontSize: "15px" }}>
+              Front End | JavaScript (ES5 and ES6), React, HTML5, CSS3, Styled-Components, SASS, Axios, AJAX <br />
+              Back End | Node.js, Express, MySQL, SQLite, MongoDB, Mongoose, PostgreSQL, PGAdmin, RESTful API Development<br />
+              Testing/Deployment | Jest, Mocha, Chai, AWS: EC2<br />
+              Developer Tools | Vim, Git, npm, yarn, Webpack, Babel, Postman
+            </p>
+          </Grid>
+          <Grid xs={4}>
+            <img className="me" src='https://i.ibb.co/93Bpmjt/Profile-Main.png' alt="self-portrait of Joy Parker" style={{ height: "140px", border: "5px solid #fff4de", borderRadius: "5px" }} />
+          </Grid>
+        </Grid>
+      </div>
     </div>
-    <div className="header-flex-1/3">
-      <img className="me" src='https://i.ibb.co/93Bpmjt/Profile-Main.png' alt="self-portrait of Joy Parker"/>
-    </div>
-  </div>
+  )
 }
 
 export default Header;
